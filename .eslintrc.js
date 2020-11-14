f343280af87fa3bcecbc3d1306e3f9ca63e747c3
xianshenglu <xianshenglu@foxmail.com>
2020-11-14 15:44:24 +0800

    chore: update eslint config to support .vue

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,37 +15,13 @@ module.exports = {
     axios: true,
     ClipboardJS: true,
   },
-  extends: [
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'airbnb-base',
-    // turn off ESLint default stylistic rules
-    // https://github.com/prettier/eslint-config-prettier/blob/master/index.js
-    'prettier',
+  extends: ['@xianshenglu/eslint-config/javascript-vue'],
+  overrides: [
+    {
+      files: './build/**/*',
+      rules: {
+        'import/no-extraneous-dependencies': 'off',
+      },
+    },
   ],
-  settings: {
-    'import/extensions': ['off'],
-  },
-  // add your custom rules here
-  rules: {
-    //! avoid add stylistic rules
-    'func-names': ['off'],
-    // non stylistic rules which are conflict with airbnb-base
-    'import/prefer-default-export': ['off'],
-    'import/no-unresolved': ['off'],
-    'import/extensions': ['off'],
-    'no-use-before-define': [
-      'error',
-      { functions: false, classes: true, variables: true },
-    ],
-    // wait for support checkForEach
-    'array-callback-return': [
-      'error',
-      { allowImplicit: true, checkForEach: true },
-    ],
-    // not compared with airbnb yet
-    'require-atomic-updates': 'error',
-    'no-implicit-coercion': 'error',
-    'no-eq-null': 'error',
-    'require-await': 'error',
-  },
 }
