7e3d03a4e9d8d6bb9bc5379c0ac07774f5086d9c
xianshenglu <xianshenglu@foxmail.com>
2020-11-17 21:22:01 +0800

    feat(request): add interceptors

--- src/apis/request.js
+++ src/apis/request.js
@@ -1,4 +1,21 @@
 import Axios from 'axios'
+import _ from 'lodash'
+
+function handleErrorResponse(error) {
+  let errorMsg = '未知错误！'
+
+  const isMessageValid =
+    _.isObject(error) &&
+    typeof error.message === 'string' &&
+    error.message.trim() !== ''
+
+  if (isMessageValid) {
+    errorMsg = error.message
+  }
+  // eslint-disable-next-line no-alert
+  alert(errorMsg)
+  return Promise.reject(error)
+}
 
 const instance = Axios.create({
   withCredentials: true,
@@ -6,5 +23,15 @@ const instance = Axios.create({
     accept: 'application/json',
   },
 })
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+  return config
+}, handleErrorResponse)
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+  return response
+}, handleErrorResponse)
 
 export default instance
